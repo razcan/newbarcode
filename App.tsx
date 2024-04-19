549800af76f9d916473bba930439d86e86dff445
razcan <razvan.mustata@gmail.com>
2024-04-19 12:28:30 +0300

    add saving style2

--- App.tsx
+++ App.tsx
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import AppNavigator from './AppNavigator';
 import { AppRegistry } from 'react-native';
-import { PaperProvider } from 'react-native-paper';
+import { PaperProvider, configureFonts, MD3LightTheme } from 'react-native-paper';
 
 export default function App() {
+
+  const fontConfig = {
+    customVariant: {
+      fontFamily: Platform.select({
+        web: 'Roboto, "Helvetica Neue", Helvetica, Arial, sans-serif',
+        ios: 'System',
+        default: 'sans-serif',
+      }),
+      fontWeight: '400',
+      letterSpacing: 0.5,
+      lineHeight: 22,
+      fontSize: 20,
+    }
+  };
+
+  const theme = {
+    ...MD3LightTheme,
+    fonts: configureFonts({ config: fontConfig }),
+  };
+
+
   return (
-    <PaperProvider>
+    <PaperProvider theme={theme}>
       <AppNavigator />
     </PaperProvider>
   );
